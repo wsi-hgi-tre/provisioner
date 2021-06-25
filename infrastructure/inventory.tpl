80344bfd4383fcfd6b715a45a0675a8eb0253549
Christopher Harrison <ch12@sanger.ac.uk>
2021-06-25 12:07:52 +0100

    Pass the username and private key to the Ansible inventory

--- infrastructure/inventory.tpl
+++ infrastructure/inventory.tpl
@@ -1,2 +1,2 @@
 [main]
-ubuntu@${floating_ip}
+ubuntu@${floating_ip} username=${username} ansible_ssh_private_key_file=${private_key}
--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -7,5 +7,7 @@ resource "local_file" "ansible_inventory" {
   file_permission = "0644"
   content         = templatefile("inventory.tpl", {
     floating_ip = openstack_networking_floatingip_v2.floating-ip.address
+    username    = var.username
+    private_key = trimsuffix(var.key, ".pub")
   })
 }
