d49f6b27b654cda67c9b696a6feedc7a6fd39afe
Christopher Harrison <ch12@sanger.ac.uk>
2021-11-03 13:42:25 +0000

    Updated default image

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -19,7 +19,7 @@ variable "flavour" {
 variable "image" {
   description = "Docker-enabled base Ubuntu Bionic image"
   type        = string
-  default     = "bionic-WTSI-docker_62820_b3f0ed74"
+  default     = "bionic-WTSI-docker_73623_ce89d144"
 }
 
 variable "username" {
